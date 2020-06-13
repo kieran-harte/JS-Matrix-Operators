fe827640e84d0d6569d86f6027d2b3266a5c896c
Kieran Harte <kieranharte@hotmail.co.uk>
2020-06-13 12:56:30 +0100

    Implement matrix add, multiply, transpose

--- src/index.js
+++ src/index.js
@@ -11,11 +11,19 @@ class Matrix {
 
   static operatorHadamard = (a, b) =>
     new Matrix(mathjs.dotMultiply(a.val, b.val))
+
+  static operatorAdd = (a, b) => new Matrix(mathjs.add(a.val, b.val))
+
+  static operatorMul = (a, b) => new Matrix(mathjs.multiply(a.val, b.val))
+
+  get T() {
+    if (Array.isArray(this.val[0])) {
+      return new Matrix(mathjs.transpose(this.val))
+    }
+    return new Matrix(mathjs.transpose([this.val]))
+  }
 }
 
 // const a = new Matrix([2, 5])
-// const b = new Matrix([4, 4])
-
-// console.log(a .* b)
 
 module.exports = Matrix
